2bc15ded96f8238d7feed37817933f0a192d2ccf
Иван Глебов <glebov.vanya8@gmail.com>
2021-11-19 20:16:40 +0300

    Added vuex storage

--- src/main.js
+++ src/main.js
@@ -2,8 +2,104 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-
-export default function (Vue, { router, head, isClient }) {
+import Vuex from 'vuex'
+export default function (Vue, { router, head, isClient, appOptions }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(Vuex)
+  appOptions.store = new Vuex.Store({
+    state: {
+      devices: [
+        {
+          id: '1',
+          name: 'Aquara temperature & humidity sensor',
+          values: [
+            {
+              name: 'Temperature',
+              value: '23.4',
+              units: '°C'
+            },
+            {
+              name: 'Humidity',
+              value: '30',
+              units: '%'
+            },
+            {
+              name: 'PM2.5',
+              value: '35',
+              units: ''
+            },
+            {
+              name: 'Air pressure',
+              value: '745.3',
+              units: 'mmHg'
+            }
+          ]
+        },
+        {
+          id: '2',
+          name: 'Aquara temperature & humidity sensor 2',
+          values: [
+            {
+              name: 'Temperature',
+              value: '23.4',
+              units: '°C'
+            },
+            {
+              name: 'Humidity',
+              value: '30',
+              units: '%'
+            },
+            {
+              name: 'PM2.5',
+              value: '35',
+              units: ''
+            },
+            {
+              name: 'Air pressure',
+              value: '745.3',
+              units: 'mmHg'
+            }
+          ]
+        },
+        {
+          id: '3',
+          name: 'Aquara temperature & humidity sensor 3',
+          values: [
+            {
+              name: 'Temperature',
+              value: '23.4',
+              units: '°C'
+            },
+            {
+              name: 'Humidity',
+              value: '30',
+              units: '%'
+            },
+            {
+              name: 'PM2.5',
+              value: '35',
+              units: ''
+            },
+            {
+              name: 'Air pressure',
+              value: '745.3',
+              units: 'mmHg'
+            }
+          ]
+        }
+      ]
+    },
+    mutations: {
+
+    },
+    getters: {
+      getDeviceById: state => deviceId => {
+        return state.devices.filter(v => v.id.toString() === deviceId.toString())
+      },
+      getDevices: state => {
+        return state.devices
+      }
+    }
+  })
 }

5d99ce76512c1330c3282b292a47bba6002a1f74
Иван Глебов <glebov.vanya8@gmail.com>
2021-11-19 20:14:42 +0300

    Added manageDevice page

--- src/templates/ManageDevice.vue
+++ src/templates/ManageDevice.vue
@@ -1,13 +1,296 @@
 <template>
-$END$
+  <Layout>
+    <div class="blockWrapper">
+      <div class="contentWrapper">
+        <div class="headerWrapper">
+          <div @click="goBack" class="backArrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="63.225" height="46.677" viewBox="0 0 63.225 46.677">
+              <g id="Component_3_1" data-name="Component 3 – 1" transform="translate(1 1.414)">
+                <path id="Path_4" data-name="Path 4" d="M150,1129.81h60.934" transform="translate(-149.709 -1107.976)"
+                      fill="none" stroke="#000" stroke-linecap="round" stroke-width="2"/>
+                <path id="Path_5" data-name="Path 5" d="M168.634,1107.976,146.709,1129.9l21.924,21.924"
+                      transform="translate(-146.709 -1107.976)" fill="none" stroke="#000" stroke-linecap="round"
+                      stroke-linejoin="round" stroke-width="2"/>
+              </g>
+            </svg>
+          </div>
+          <div class="header">{{ deviceName }}</div>
+        </div>
+        <div class="deviceCardWrapper">
+          <div class="deviceImageWrapper">
+            <g-image alt="device img"/>
+          </div>
+          <div class="deviceDataWrapper">
+            <div class="buttonsContainer">
+              <button @click="setSimpleMode" :class="{activeButton: tableMode}">Simple</button>
+              <button @click="setTableMode" :class="{activeButton: !tableMode}">Table</button>
+            </div>
+            <div class="simpleModeWrapper" v-if="tableMode">
+              <ValuesRow :values="device.values"/>
+            </div>
+            <div class="tableModeWrapper" v-if="!tableMode">
+              <Table :rows="device.values"/>
+<!--              <div class="tableWrapper">-->
+<!--                <div v-for="item in device.values" class="tableRow">-->
+<!--                  <div class="col1">{{ item.name }}</div>-->
+<!--                  <div class="col2">{{ item.value }}{{ item.units }}</div>-->
+<!--                </div>-->
+<!--              </div>-->
+              <div @click="testAlert" class="rowDataUpdateButton">
+                <svg xmlns="http://www.w3.org/2000/svg" width="37.32" height="31.51" viewBox="0 0 37.32 31.51">
+                  <g id="Component_1_1" data-name="Component 1 – 1" transform="translate(1.215 1)">
+                    <g id="Group_24" data-name="Group 24" transform="translate(-70.96 -21)">
+                      <g id="Group_23" data-name="Group 23" transform="translate(-924.217 -496.533)">
+                        <g id="Group_21" data-name="Group 21" transform="translate(997.962 535.012)">
+                          <path id="Path_15" data-name="Path 15" d="M1023.925,544.441l6.027-2.907,1.645,6.486"
+                                transform="translate(-999.491 -541.533)" fill="none" stroke="#1547d0"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="2"/>
+                          <path id="Path_16" data-name="Path 16" d="M997.807,543.5a16.1,16.1,0,0,0,29.448,0"
+                                transform="translate(-997.807 -541.058)" fill="none" stroke="#1547d0"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="2"/>
+                        </g>
+                        <g id="Group_22" data-name="Group 22" transform="translate(995.178 517.533)">
+                          <path id="Path_17" data-name="Path 17" d="M1001.84,521.508l-6.31,3.044-1.722-6.791"
+                                transform="translate(-993.807 -513.454)" fill="none" stroke="#1547d0"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="2"/>
+                          <path id="Path_18" data-name="Path 18" d="M1027.827,527.122a16.1,16.1,0,0,0-29.447,0"
+                                transform="translate(-995.923 -517.533)" fill="none" stroke="#1547d0"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="2"/>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div class="controlDeviceWrapper">
+              <div class="buttonsContainer">
+                <div class="switchElement">Control your device</div>
+              </div>
+              <div class="inputWrapper">
+                <input placeholder="Your message">
+              </div>
+              <button @click="testAlert" class="sendButton">
+                <div>Send message</div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="33.712" height="25.54" viewBox="0 0 33.712 25.54">
+                  <g id="Group_9" data-name="Group 9" transform="translate(1 1.414)">
+                    <path id="Path_9" data-name="Path 9" d="M181.561,1129.81H150" transform="translate(-150 -1118.501)"
+                          fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2"/>
+                    <path id="Path_10" data-name="Path 10" d="M146.709,1107.976l11.356,11.356-11.356,11.356"
+                          transform="translate(-126.354 -1107.976)" fill="none" stroke="#fff" stroke-linecap="round"
+                          stroke-linejoin="round" stroke-width="2"/>
+                  </g>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
 </template>
-
 <script>
-export default {
-name: "ManageDevice"
-}
-</script>
 
-<style scoped>
+import { mapGetters } from "vuex";
+import ValuesRow from "../components/ValuesRow";
+import Table from '../components/Table';
+
+export default {
+  name: "ManageDevice",
+  components: {Table, ValuesRow},
+  data: () => {
+    return {
+      deviceName: 'Aquara temperature & humidity sensor',
+      tableMode: true,
+      device: {}
+    }
+  },
+  computed: {
+    ...mapGetters(['getDeviceById'])
+  },
+  mounted () {
+    this.device = this.getDeviceById(this.$route.params.deviceId)[0]
+  },
+  methods: {
+    testAlert () {
+      alert('test message')
+    },
+    setSimpleMode () {
+      this.tableMode = true
+    },
+    setTableMode () {
+      this.tableMode = false
+    },
+    goBack () {
+      this.$router.push('/')
+    },
+    showId () {
+      console.log(this.$route.params.deviceId)
+    }
+  }
+}
+
+</script>
+<style lang="css" scoped>
+
+.blockWrapper {
+  height: calc(80vh - 205px);
+  padding: 53px;
+}
+
+.headerWrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  align-items: center;
+}
+
+.backArrow {
+  cursor: pointer;
+}
+
+.header {
+  font-family: 'Raleway', sans-serif;
+  text-wrap: normal;
+  font-size: 2.25rem;
+  font-weight: 900;
+  line-height: 1.25;
+}
+
+.contentWrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 39px;
+}
+
+
+.deviceCardWrapper {
+  display: flex;
+}
+
+.deviceImageWrapper {
+  width: 30%;
+}
+
+.deviceDataWrapper {
+  width: 70%;
+}
+
+.buttonsContainer {
+  padding: 0;
+  margin: 0;
+  border-bottom: 1px solid var(--main-black-color);
+  justify-content: flex-end;
+  align-items: flex-end;
+}
+
+button {
+  position: relative;
+  top: 1px;
+  width: 74px;
+  height: 25px;
+  background-color: transparent;
+  border: 1px solid var(--main-black-color);
+  cursor: pointer;
+  font-size: 0.875rem;
+  padding: 0;
+  margin: 0;
+}
+
+.switchElement {
+  position: relative;
+  display: inline-block;
+  height: 25px;
+  color: var(--main-white-color);
+  background-color: var(--main-black-color);
+  border: 1px solid var(--main-black-color);
+  cursor: pointer;
+  font-family: 'Roboto Mono', Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace;
+  font-size: 0.875rem;
+  padding: 0 12px;
+  margin: 0;
+}
+
+
+.activeButton {
+  background-color: var(--main-black-color);
+  color: var(--main-white-color);
+}
+
+.simpleModeWrapper {
+  margin-top: 15px;
+}
+
+.tableModeWrapper {
+  margin-top: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.tableWrapper {
+  max-height: 100px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 4px 14px 14px;
+  border: 1px solid var(--main-black-color);
+  width: 70%;
+}
+
+.tableRow {
+  display: flex;
+  flex-direction: row;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid black;
+}
+
+.tableRow:last-child {
+  border-bottom: none;
+}
+
+.col1 {
+  width: 50%;
+}
+
+.col2 {
+  width: 50%;
+  text-align: left;
+}
+
+.controlDeviceWrapper {
+  margin-top: 58px;
+}
+
+.inputWrapper input {
+  border: 1px solid var(--main-black-color);
+  border-radius: 0;
+  margin-top: 15px;
+  width: calc(100% - 30px);
+  padding: 10px 14px 14px 14px;
+  font-size: 1.25rem;
+}
+
+.sendButton {
+  cursor: pointer;
+  margin-top: 9px;
+  border: 0;
+  font-size: 1.25rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--main-blue-color);
+  color: var(--main-white-color);
+  padding: 13px 21px 13px 15px;
+  height: auto;
+}
+
+
 
 </style>

4e8891ba20f01058a299c3561b428ad225f38c91
Иван Глебов <glebov.vanya8@gmail.com>
2021-11-19 20:14:03 +0300

    Added device card component

--- src/components/DeviceCard.vue
+++ src/components/DeviceCard.vue
@@ -1,13 +1,79 @@
 <template>
-$END$
+  <div class="deviceWrapper">
+    <div class='deviceImage'>
+      <g-image :src="imgSrc" alt="device image"/>
+    </div>
+    <div class='deviceStats'>
+      <div class='deviceName'>{{ name }}</div>
+      <span class='splitter'></span>
+      <ValuesRow :values="values"/>
+      <div>
+        <g-link :to="`/device/` + $props.deviceId" class="link">Manage</g-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ValuesRow from "./ValuesRow";
 export default {
-name: "DeviceCard"
+  name: "DeviceCard",
+  components: {ValuesRow},
+  props: {
+    deviceId: String,
+    name: String,
+    imgSrc: String,
+    values: [
+      {
+        name: String,
+        value: Number,
+        units: String
+      }
+    ]
+  },
+  methods: {
+    testAlert: () => {
+      alert('test click')
+    }
+  }
 }
+
 </script>
 
 <style scoped>
+
+.deviceWrapper {
+  /*font-family: 'Roboto Mono', Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace;*/
+  padding: 24px 22px 32px 18px;
+  border: 2px solid var(--main-black-color);
+  display: flex;
+  gap: 20px;
+}
+
+.deviceName {
+  font-weight: bold;
+  font-size: 1.25rem;
+}
+
+.deviceImage {
+  width: 150px;
+}
+
+.deviceStats {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+
+.link {
+  color: var(--main-blue-color);
+  text-decoration: none;
+}
+
+.link:visited {
+  color: var(--main-blue-color);
+}
 
 </style>

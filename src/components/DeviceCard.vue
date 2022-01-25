66dc59febfd651382799f90adb83261545caaf41
Иван Глебов <glebov.vanya8@gmail.com>
2022-01-25 19:32:32 +0300

    Added checkbox for enabling/disabling data writing into datalog

--- src/components/DeviceCard.vue
+++ src/components/DeviceCard.vue
@@ -12,16 +12,35 @@
       <div v-if="device.isManageable">
         <g-link :to="`/device/` + device.id" class="link">Manage</g-link>
       </div>
+      <div class="checkboxUpdate">
+        <Checkbox v-model="checkboxState" @change="onCheckboxChange">Enable writing data into datalog</Checkbox>
+        <Button v-if="showUpdateButton" class="checkboxUpdateButton" @click="toggleModal" color-scheme="regular">Update</Button>
+      </div>
+      <Modal :show="modalShow">
+        <div>
+          <h3>Sid phrase is required to proceed</h3>
+          <Input type="password" v-model="seedPhrase" placeholder="Enter your sid phrase"/>
+          <div class="control-buttons">
+            <Button color-scheme="regular" @click="toggleModal">Cancel</Button>
+            <Button color-scheme="regular" @click="launchDevice">Fetch data</Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   </div>
 </template>
 
 <script>
 import ValuesRow from "./ValuesRow";
+import Checkbox from "./Checkbox";
+import axios from "axios";
+import Modal from "./Modal";
+import Input from "./Input";
+import Button from "./Button";
 
 export default {
   name: "DeviceCard",
-  components: {ValuesRow},
+  components: {Checkbox, ValuesRow, Modal, Button, Input},
   props: {
     device: {
       id: String,
@@ -36,7 +55,49 @@ export default {
       ],
       isManageable: Boolean
     }
-  }
+  },
+  data: () => {
+    return {
+      checkboxState: false,
+      modalShow: false,
+      seedPhrase: '',
+      test: false,
+      showUpdateButton: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.$data.modalShow = !this.$data.modalShow
+    },
+    onCheckboxChange () {
+      this.$data.showUpdateButton = !this.$data.showUpdateButton
+    },
+    launchDevice() {
+      if(this.$data.seedPhrase !== undefined && this.$data.seedPhrase !== '') {
+        axios.post('http://localhost:8000/launchDevice', {
+          id: this.$props.device.id,
+          seed: this.$data.seedPhrase,
+          state: this.$data.checkboxState
+        })
+            .then(res => {
+              if (res.status === 200) {
+                if (res.data.status === 200) {
+                  this.toggleModal()
+                  this.$data.showUpdateButton = false
+                } else {
+                  alert(`Something has gone wrong while writing changes into datalog. Try again later\n ${res.data.message}`)
+                }
+              } else {
+                alert("Something has gone wrong while writing changes into datalog. Try again later")
+              }
+            })
+            .catch(error => {
+              alert(`Something has gone wrong while writing changes into datalog. Try again later\n ${error}`)
+
+            })
+      }
+    }
+  },
 }
 
 </script>
@@ -86,6 +147,21 @@ export default {
 
 .link:visited {
   color: var(--main-blue-color);
+}
+
+.control-buttons {
+  padding-top: 20px;
+}
+
+.checkboxUpdate {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.checkboxUpdateButton {
+
+  margin-right: -10px;
 }
 
 @media screen and (min-width: 1700px) {
